395cfd2a275a61e3ee4d572f456bfb8470c765e5
Huy Phan <huyphan520@gmail.com>
2023-01-03 10:16:45 -0600

    IC 4 patron test start

---
+++ spec/patron_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/patron'
+
+RSpec.describe Patron do
+
+    describe "#initialize" do
+        it 'exists' do
+            patron_1 = Patron.new("Bob", 20)
+
+            expect(patron_1).to be_instance_of(Patron)
+        end
+
+        it 'has attributes' do
+            patron_1 = Patron.new("Bob", 20)
+            
+            expect(patron_1.name).to eq("Bob")
+            expect(patron_1.spending_money).to eq(20)
+            expect(patron_1.interests).to eq([])
+        end
+    end
+end
