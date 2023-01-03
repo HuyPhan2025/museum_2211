8e941f4a2a75db2eb7c1894692c9d6c9c3fce0f7
Huy Phan <huyphan520@gmail.com>
2023-01-03 10:07:49 -0600

    IC 4 start

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,20 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
+
+    describe '#initialize' do
+        it 'exists' do
+            exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+            expect(exhibit).to be_instance_of(Exhibit)
+        end
+
+        it 'has attributes' do
+            exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+            expect(exhibit.name).to eq("Gems and Minerals")
+            expect(exhibit.cost).to eq(0)
+        end
+    end
   
 end
